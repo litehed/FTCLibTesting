75045e1c2a1469f80201fedd1337f337d0ffb793
litehed <57595571+litehed@users.noreply.github.com>
2021-07-28 15:11:19 -0400

    ITS FOLLOWING A TRAJECTORY (poorly)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpmode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.ProfiledPIDController;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
 import com.arcrobotics.ftclib.trajectory.Trajectory;
@@ -17,6 +18,18 @@ import org.firstinspires.ftc.teamcode.additions.HolonomicDriveController;
 @Autonomous
 public class TestOpmode extends LinearOpMode {
 
+    public static double TRACKWIDTH = 0.38227;
+    public static double CENTER_WHEEL_OFFSET = -0.172212;
+    public static double WHEEL_DIAMETER = (0.688976 * 2)/39.37;
+    public static double TICKS_PER_REV = 8192;
+    public static double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER / TICKS_PER_REV;
+
+    private Motor.Encoder leftOdometer, rightOdometer, midOdometer;
+
+    public void sugma(){
+        telemetry.addData("What's", " sugma?");
+        telemetry.update();
+    }
     @Override
     public void runOpMode() throws InterruptedException {
         Motor frontLeft = new Motor(hardwareMap, "fL");
@@ -27,8 +40,16 @@ public class TestOpmode extends LinearOpMode {
         backLeft.setInverted(true);
         frontLeft.setInverted(true);
 
-        backRight.encoder.setDirection(Motor.Direction.REVERSE);
-        frontLeft.encoder.setDirection(Motor.Direction.REVERSE);
+        leftOdometer = frontRight.encoder;
+        rightOdometer = backLeft.encoder;
+        midOdometer = frontLeft.encoder;
+
+        leftOdometer.setDirection(Motor.Direction.REVERSE);
+        midOdometer.setDirection(Motor.Direction.REVERSE);
+
+        leftOdometer.setDistancePerPulse(DISTANCE_PER_PULSE);
+        rightOdometer.setDistancePerPulse(DISTANCE_PER_PULSE);
+        midOdometer.setDistancePerPulse(DISTANCE_PER_PULSE);
 
         PIDController translationController = new PIDController(1, 0, 0);
         ProfiledPIDController headingController = new ProfiledPIDController(1, 0, 0,
@@ -36,29 +57,36 @@ public class TestOpmode extends LinearOpMode {
         HolonomicDriveController holonomicDriveController =
                 new HolonomicDriveController(translationController, translationController, headingController);
         MecanumDrive mecanumDrive = new MecanumDrive(
+                false,
                 frontLeft,
                 frontRight,
                 backLeft,
                 backRight
         );
         HolonomicOdometry holonomicOdometry = new HolonomicOdometry(
-                backLeft::getCurrentPosition,
-                backRight::getCurrentPosition,
-                frontLeft::getCurrentPosition,
-                15.05,
-                -6.78
+                frontRight::getDistance,
+                backLeft::getDistance,
+                frontLeft::getDistance,
+                TRACKWIDTH,
+                CENTER_WHEEL_OFFSET
         );
         MecanumTrajectoryFollower trajectoryFollower = new MecanumTrajectoryFollower(
                 mecanumDrive,
                 holonomicOdometry,
                 holonomicDriveController
         );
+
+        backLeft.resetEncoder();
+        frontRight.resetEncoder();
+        frontLeft.resetEncoder();
+
         Trajectory traj = TestTrajectory.generateTrajectory();
-        telemetry.addData("Total Time Seconds", traj.getTotalTimeSeconds());
-        telemetry.update();
         waitForStart();
         trajectoryFollower.followTrajectory(traj);
-
+        while(!isStopRequested()){
+            telemetry.addData("Pose: ", holonomicOdometry.getPose());
+            telemetry.update();
+        }
     }
 }
 
